86ea7f2c8c6aeec3741ad24bfe435fd55179ba1a
Aldair <adrianaaylinmaldonadosiles@gmail.com>
2026-05-22 00:27:40 -0400

    Diseño premium ALDRSTORE

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import "./globals.css";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,9 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
